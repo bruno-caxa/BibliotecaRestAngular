a1bc43c4ff160ddfedd56cb2455517b3ad5bbc9c
bruno-caxa <frian6123@gmail.com>
2023-05-31 15:36:15 -0300

    31/05/23

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,7 +1,7 @@
 const PROXY_CONFIG = [
   {
     context: ['/api'],
-    target: 'https://bibliotecarestspring-production.up.railway.app/',
+    target: 'http://bibliotecarestspring-production.up.railway.app/',
     secure: false,
     logLevel: 'debug'
   }
